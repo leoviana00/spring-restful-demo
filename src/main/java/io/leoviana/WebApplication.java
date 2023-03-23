8917d411e073ef751ac99c8d78f25c09e9fb7005
Leonardo Viana <leonardoviana00@hotmail.com>
2023-03-23 18:23:13 -0300

    feat(spring): application.yaml config ext

--- src/main/java/io/leoviana/WebApplication.java
+++ src/main/java/io/leoviana/WebApplication.java
@@ -2,6 +2,7 @@ package io.leoviana;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class WebApplication {
-    @Autowired
-    @Qualifier("webApplicationName")
+    @Value("${spring.app.name}")
     private String webApplicationName;
     @GetMapping("/hello")
     public String helloWorld(){
         return webApplicationName;
     }
     public static void main(String[] args) {
+
         SpringApplication.run(WebApplication.class, args);
     }
 }
--- src/main/java/io/leoviana/WebConfiguration.java
+++ src/main/java/io/leoviana/WebConfiguration.java
@@ -5,8 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfiguration {
-    @Bean(name = "webApplicationName")
-    public String webApllicationName(){
-        return "<h1 style=\"color: black; font-size: 40px; font: cursive;\">Utilizando o Bean para realizar configurações 💻 </h1>";
-    }
+
 }
