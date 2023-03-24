fba1d44e8f7793f91dd7219a36a52177dfcc13e7
Leonardo Viana <leonardoviana00@hotmail.com>
2023-03-23 22:41:36 -0300

    feat(spring): multiplos ambientes e profiles

--- src/main/java/io/leoviana/WebConfiguration.java
+++ src/main/java/io/leoviana/WebConfiguration.java
@@ -1,9 +1,17 @@
 package io.leoviana;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class WebConfiguration {
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO EM AMBIENTE DE DESENVOLVIMENTO");
+        };
+    }
 }
